a4b903bd984145d8c8af812ec9ac100ff919cff4
Muhammad Burhanudin <mburhanudin149@gmail.com>
2026-09-26 17:30:02 +0700

    feat: remove native splash logo and use custom animated Flutter splash screen only

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     super.initState();
     _animController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 650),
+      duration: const Duration(milliseconds: 850),
     );
 
     _scaleAnimation = CurvedAnimation(
@@ -35,7 +35,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
     _animController.forward();
 
-    _redirectTimer = Timer(const Duration(milliseconds: 850), () {
+    _redirectTimer = Timer(const Duration(milliseconds: 1500), () {
       if (mounted) {
         Navigator.pushReplacementNamed(context, '/home');
       }
